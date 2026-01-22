0db0023a4a1f538af1a86c5880a1b596c0d7a168
aryan2064 <aryanawasthi1974@gmail.com>
2026-01-22 19:42:15 +0530

    add click-to-zoom functionality on canvas

--- script.js
+++ script.js
@@ -92,4 +92,25 @@ function drawMandelbrot() {
     ctx.putImageData(image, 0, 0);
 }
 
+canvas.addEventListener("click", function (event) {
+    var rect = canvas.getBoundingClientRect();
+    var mouseX = event.clientX - rect.left;
+    var mouseY = event.clientY - rect.top;
+
+    var clickedX = minX + (mouseX / width) * (maxX - minX);
+    var clickedY = minY + (mouseY / height) * (maxY - minY);
+
+    var zoomFactor = 0.5;
+
+    var rangeX = (maxX - minX) * zoomFactor;
+    var rangeY = (maxY - minY) * zoomFactor;
+
+    minX = clickedX - rangeX / 2;
+    maxX = clickedX + rangeX / 2;
+    minY = clickedY - rangeY / 2;
+    maxY = clickedY + rangeY / 2;
+
+    drawMandelbrot();
+});
+
 drawMandelbrot();
